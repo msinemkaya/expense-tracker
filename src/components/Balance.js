ae0491d49679e8d33388248ec3aad35ed89b5339
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-08 20:43:15 +0300

    feat: filled the Balace component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,16 @@
+import useGlobalContext from '../hooks/use-global-context';
+
 export default function Balance(){
+
+  const { state } = useGlobalContext()
+
+  const amounts = state.transactions.map(transaction => transaction.amount)
+  const total = amounts.reduce((acc, crr) => (acc += crr), 0).toFixed(2)
+
   return(
     <>
       <h4>Your Balance</h4>
-      <h1 id='balance'></h1>
+      <h1>${total}</h1>
     </>
   );
 }
