66d86160af0a9565b51b4d304891dcc2f9e28075
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-08 17:50:34 +0300

    feat: created Balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
 import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <>
       <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,8 @@
+export default function Balance(){
+  return(
+    <>
+      <h4>Your Balance</h4>
+      <h1 id='balance'></h1>
+    </>
+  );
+}
